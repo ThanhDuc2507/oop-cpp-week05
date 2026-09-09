084f970ccc529c687e8b9c1885448bf3e60acf8f
Huynh Nguyen Thanh Duc <25tm14503@student.hcmute.edu.vn>
2026-09-09 09:33:06 +0700

    Hi

--- main.cpp
+++ main.cpp
@@ -4,8 +4,6 @@ using namespace std;
 int main() {
     int n;
     double scores[20];
-    int pass = 0;
-    int fail = 0;
 
     cout << "How many students? ";
     cin >> n;
@@ -15,21 +13,21 @@ int main() {
         cin >> scores[i];
     }
 
-    for (int i = 0; i < n; i++) {
-        if (scores[i] >= 5) {
-            pass++;
-        } else {
-            fail++;
+    double highest = scores[0];
+    double lowest = scores[0];
+
+    for (int i = 1; i < n; i++) {
+        if (scores[i] > highest) {
+            highest = scores[i];
+        }
+
+        if (scores[i] < lowest) {
+            lowest = scores[i];
         }
     }
 
-    cout << "===== STATISTICS =====" << endl;
-    cout << "Passed: " << pass << " students" << endl;
-    cout << "Failed: " << fail << " students" << endl;
-
-    double passRate = (double)pass / n * 100;
-
-    cout << "Pass rate: " << passRate << "%" << endl;
+    cout << "Highest score: " << highest << endl;
+    cout << "Lowest score: " << lowest << endl;
 
     return 0;
 }
