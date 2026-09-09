2d34d0039836d920f9b178ed79beafb44153c43b
Huynh Nguyen Thanh Duc <25tm14503@student.hcmute.edu.vn>
2026-09-09 09:06:11 +0700

    hello

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    cout << "Hello World!" << endl;
+int main() {
+    int n;
+
+    do {
+        cout << "How many students? ";
+        cin >> n;
+
+        if (n < 2 || n > 20) {
+            cout << "Invalid number of students!" << endl;
+            cout << "Please enter a number from 2 to 20." << endl;
+        }
+
+    } while (n < 2 || n > 20);
 
     return 0;
 }
