52236b63776a0b94115ee9ae2a0b8e06ec02f1f1
Huynh Nguyen Thanh Duc <25tm14503@student.hcmute.edu.vn>
2026-09-09 09:11:34 +0700

    hello

--- main.cpp
+++ main.cpp
@@ -3,17 +3,22 @@ using namespace std;
 
 int main() {
     int n;
+    double scores[20];
 
-    do {
-        cout << "How many students? ";
-        cin >> n;
+    cout << "How many students? ";
+    cin >> n;
 
-        if (n < 2 || n > 20) {
-            cout << "Invalid number of students!" << endl;
-            cout << "Please enter a number from 2 to 20." << endl;
-        }
+    for (int i = 0; i < n; i++) {
+        do {
+            cout << "Enter score for student " << i + 1 << ": ";
+            cin >> scores[i];
 
-    } while (n < 2 || n > 20);
+            if (scores[i] < 0 || scores[i] > 10) {
+                cout << "Invalid score! Please enter a value from 0 to 10." << endl;
+            }
+
+        } while (scores[i] < 0 || scores[i] > 10);
+    }
 
     return 0;
 }
