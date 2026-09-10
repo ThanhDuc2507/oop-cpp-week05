48e68a3a368b506734865742891f3a1fde0581e5
Huynh Nguyen Thanh Duc <25tm14503@student.hcmute.edu.vn>
2026-09-10 21:47:18 +0700

    hello

--- main.cpp
+++ main.cpp
@@ -3,19 +3,11 @@ using namespace std;
 
 int main() {
     float scores[10] = {7.5, 8.0, 6.5, 9.5, 7.0, 9.0, 5.5, 7.5, 8.5, 4.5};
-    bool found = false;
 
-    for (int i = 0; i < 10; i++) {
-        if (scores[i] < 5.0) {
-            cout << "First failing student: Student " << i + 1 << endl;
-            cout << "Score: " << scores[i];
-            found = true;
-            break;
+    for (int i = 1; i < 10; i++) {
+        if (scores[i] > scores[i - 1]) {
+            cout << "Student " << i + 1 << " improved compared to Student " << i << endl;
         }
-    }
-
-    if (!found) {
-        cout << "All students pass!";
     }
 
     return 0;
