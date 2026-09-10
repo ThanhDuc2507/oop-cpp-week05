60cfe51b5a038b5837eb0b25570374d399fc5896
Huynh Nguyen Thanh Duc <25tm14503@student.hcmute.edu.vn>
2026-09-10 21:50:55 +0700

    hello

--- main.cpp
+++ main.cpp
@@ -4,22 +4,32 @@ using namespace std;
 int main() {
     float scores[10] = {7.5, 8.0, 6.5, 9.5, 7.0, 9.0, 5.5, 7.5, 8.5, 4.5};
 
-    int currentStreak = 0;
-    int longestStreak = 0;
+    int range1 = 0;
+    int range2 = 0;
+    int range3 = 0;
+    int range4 = 0;
+    int range5 = 0;
 
     for (int i = 0; i < 10; i++) {
-        if (scores[i] > 5.0) {
-            currentStreak++;
-
-            if (currentStreak > longestStreak) {
-                longestStreak = currentStreak;
-            }
+        if (scores[i] < 5.0) {
+            range1++;
+        } else if (scores[i] < 6.5) {
+            range2++;
+        } else if (scores[i] < 8.0) {
+            range3++;
+        } else if (scores[i] < 9.0) {
+            range4++;
         } else {
-            currentStreak = 0;
+            range5++;
         }
     }
 
-    cout << "Longest pass streak: " << longestStreak << " students";
+    cout << "----- SCORE DISTRIBUTION -----" << endl;
+    cout << "0 - <5    : " << range1 << endl;
+    cout << "5 - <6.5  : " << range2 << endl;
+    cout << "6.5 - <8  : " << range3 << endl;
+    cout << "8 - <9    : " << range4 << endl;
+    cout << "9 - 10    : " << range5 << endl;
 
     return 0;
 }
