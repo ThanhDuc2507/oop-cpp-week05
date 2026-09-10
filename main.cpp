75093017a33c9ffebebdff6b12c87383b39c8600
Huynh Nguyen Thanh Duc <25tm14503@student.hcmute.edu.vn>
2026-09-10 21:05:49 +0700

    Helo

--- main.cpp
+++ main.cpp
@@ -3,15 +3,22 @@ using namespace std;
 
 int main() {
     float scores[10] = {7.5, 8.0, 6.5, 9.5, 7.0, 9.0, 5.5, 7.5, 8.5, 4.5};
+    float a, b;
     int count = 0;
 
+    cout << "Enter minimum score: ";
+    cin >> a;
+
+    cout << "Enter maximum score: ";
+    cin >> b;
+
     for (int i = 0; i < 10; i++) {
-        if (scores[i] >= 9.0) {
+        if (scores[i] >= a && scores[i] <= b) {
             count++;
         }
     }
 
-    cout << "Number of excellent students: " << count;
+    cout << "Students in range: " << count;
 
     return 0;
 }
