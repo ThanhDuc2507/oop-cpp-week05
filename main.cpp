d0849d4b3ef4a331885e9d56fb3c5c25ae05723e
Huynh Nguyen Thanh Duc <25tm14503@student.hcmute.edu.vn>
2026-09-10 21:43:24 +0700

    hello

--- main.cpp
+++ main.cpp
@@ -3,19 +3,20 @@ using namespace std;
 
 int main() {
     float scores[10] = {7.5, 8.0, 6.5, 9.5, 7.0, 9.0, 5.5, 7.5, 8.5, 4.5};
-    float x;
-    int count = 0;
-
-    cout << "Enter score: ";
-    cin >> x;
+    bool found = false;
 
     for (int i = 0; i < 10; i++) {
-        if (scores[i] == x) {
-            count++;
+        if (scores[i] < 5.0) {
+            cout << "First failing student: Student " << i + 1 << endl;
+            cout << "Score: " << scores[i];
+            found = true;
+            break;
         }
     }
 
-    cout << "Score " << x << " appears " << count << " times.";
+    if (!found) {
+        cout << "All students pass!";
+    }
 
     return 0;
 }
