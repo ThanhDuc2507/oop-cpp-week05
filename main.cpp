8afd1a55a2b70b949c988ee77a6976bc0a94886e
Huynh Nguyen Thanh Duc <25tm14503@student.hcmute.edu.vn>
2026-09-10 21:48:50 +0700

    hello

--- main.cpp
+++ main.cpp
@@ -4,11 +4,22 @@ using namespace std;
 int main() {
     float scores[10] = {7.5, 8.0, 6.5, 9.5, 7.0, 9.0, 5.5, 7.5, 8.5, 4.5};
 
-    for (int i = 1; i < 10; i++) {
-        if (scores[i] > scores[i - 1]) {
-            cout << "Student " << i + 1 << " improved compared to Student " << i << endl;
+    int currentStreak = 0;
+    int longestStreak = 0;
+
+    for (int i = 0; i < 10; i++) {
+        if (scores[i] > 5.0) {
+            currentStreak++;
+
+            if (currentStreak > longestStreak) {
+                longestStreak = currentStreak;
+            }
+        } else {
+            currentStreak = 0;
         }
     }
+
+    cout << "Longest pass streak: " << longestStreak << " students";
 
     return 0;
 }
